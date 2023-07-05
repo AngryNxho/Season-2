2bbc1856d2436945154cf616785c627c49495864
AngryNxho <108508244+AngryNxho@users.noreply.github.com>
2023-07-05 03:10:25 -0400

    das

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -10,38 +10,29 @@ function twoNumbers(arrOne, arrTwo){
     let total = [];
     let acumulator = 0;
     backup = 0;
+    let x;
 
-    for (let x = 0; x < firstArray.length; x++){
-        if (((firstArray[x] && secondArray[x]) >= 0)
-        && (firstArray[x] && secondArray[x]) <= 9){
-            sum += firstArray[x] + secondArray[x]; 
-        }
-    }
 
-    for (let i = 1; i <= sum.length; i++){
-        if ((sum[sum.length - i] <= 9) && (sum[sum.length - i] > 0)){
-            total.push(sum[sum.length - i]);
+    for (x = 0; x < firstArray.length; x++){
+        if (firstArray[x] + secondArray[x] <= 9 
+        && firstArray[x] + secondArray[x] > 0){
+            sum.push(firstArray[x] + secondArray[x]);
         }else{
+            sum.push(0);
             acumulator += 1;
         }
     }
 
-    for(let z = 0; z <= acumulator; z++){
-        if (total[z] == 1){
-            total[z] = 0;
-            backup = total[total.length - 1];
-            total.pop();
+
+    for (i of sum){
+        if (i == 0){
+            i = 0;
         }
     }
 
 
-    total.push(Number(backup) + 1)
-
-
-    console.log(acumulator);
-
     
-   return total;
+   return "";
 
 }
 
@@ -49,8 +40,7 @@ function twoNumbers(arrOne, arrTwo){
 
 
 console.log(twoNumbers([2,4,3], [5,6,4]));
-console.log(twoNumbers([5,4,3], [5,6,4]));
+// console.log(twoNumbers([5,4,3], [5,6,4]));
+// console.log(twoNumbers([5,4,6], [5,6,4]));
 
 
-
-console.log();
