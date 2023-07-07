f6778ed874658e1a99feb5c7aab23dd58b9c20a8
AngryNxho <pleasetrix@gmail.com>
2023-07-06 23:17:43 -0400

    hoal

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -2,8 +2,7 @@ function twoNumbers(arrOne, arrTwo) {
   const firstArray = arrOne;
   const secondArray = arrTwo;
 
-  firstArray.reverse();
-  secondArray.reverse();
+  
 
   let len = 0;
   let total = [];
@@ -12,15 +11,21 @@ function twoNumbers(arrOne, arrTwo) {
     ? len = firstArray.length 
     : len = secondArray.length;
   
-
+  console.log(firstArray.length);
+  console.log(secondArray.length);
   for (x = 0; x < len; x++){
-    console.log(firstArray[x] + secondArray[x]);
-    total.push(firstArray[x] + secondArray[x]);
+    if (firstArray[x] + secondArray[x] >= 0){
+      total.push(firstArray[x] + secondArray[x]);
+    }else{
+        (firstArray  == len || secondArray == len) 
+        ? total.push(0 + firstArray[firstArray.length - 1])
+        : total.push(0 + secondArray[secondArray.length - 1])
+      
+    }
   }
-
-  total.pop();
   return total;
 }
 
 console.log(twoNumbers([1, 2, 3], [1, 2, 3, 4]));
+console.log(twoNumbers([1, 2, 3, 4], [1, 2, 3, ]));
 // console.log(twoNumbers([5,4,6], [5,6,4]));
