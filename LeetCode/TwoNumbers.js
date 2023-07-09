9537cc3882f56cb515b734f953dbcd1af7cdfe11
AngryNxho <108508244+AngryNxho@users.noreply.github.com>
2023-07-09 13:29:20 -0400

    sdafasdfsd

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -1,31 +1,32 @@
 function twoNumbers(arrOne, arrTwo) {
   const firstArray = arrOne;
   const secondArray = arrTwo;
-  let sum = 0;
 
   let len = 0;
   let total = [];
-
- 
-  (firstArray.length > secondArray.length)
+  
+  firstArray.length > secondArray.length
     ? len = firstArray.length
     : len = secondArray.length
 
+  
 
   for (let x = 0; x < len; x++){
-    operator = firstArray[x] + secondArray[x];
-    total.push(operator)
+    if (firstArray[x] === undefined){
+      firstArray[x] = 0;
+    }
+    else if (secondArray[x] === undefined){
+      secondArray[x] = 0;
+    }
+    total.push(firstArray[x] + secondArray[x])
   }
 
+for (let z of total){
+  console.log(z);
+}
 
-  for (let i of total){
-    while (i.length)
-  }
-
-    
+  console.log(total);
   return "";
 }
 
-console.log(twoNumbers([1, 2, 3], [1, 8, 6])); // [2, 10, 9] ||  [2, 0, 0, 1] == [1, 0, 0, 2]
-// console.log(twoNumbers([1, 2, 3, 4], [1, 2,4,5,6,8]));
-// console.log(twoNumbers([5,4,6], [5,6,4]));
+console.log(twoNumbers([1, 2, 3], [1, 8, 6, 2])); // [2, 10, 9] ==  [2, 0, 0, 1] 
--- hola.js
+++
@@ -1,9 +0,0 @@
-let i= [10];
-let total = [];
-let acum = 1;
-
-for (x of i){
-}
-
-
-console.log(total);
