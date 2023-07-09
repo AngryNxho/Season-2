bb951ee226edba633b2551cdf664954a3382810e
AngryNxho <pleasetrix@gmail.com>
2023-07-09 13:54:06 -0400

    af

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -3,8 +3,9 @@ function twoNumbers(arrOne, arrTwo) {
   const secondArray = arrTwo;
 
   let len = 0;
+  let check = [];
   let total = [];
-  
+  let acumulator = 0;
   firstArray.length > secondArray.length
     ? len = firstArray.length
     : len = secondArray.length
@@ -18,15 +19,18 @@ function twoNumbers(arrOne, arrTwo) {
     else if (secondArray[x] === undefined){
       secondArray[x] = 0;
     }
-    total.push(firstArray[x] + secondArray[x])
+    check.push(firstArray[x] + secondArray[x])
   }
 
-for (let z of total){
-  console.log(z);
+for (let z = 0; z < check.length; z++){
+  if (check[z] > 9){
+    check[z] = 0;
+    total.push(check[z])
+    acumulator++;
+  }
 }
-
-  console.log(total);
-  return "";
+    console.log(acumulator);
+    return "";
 }
 
 console.log(twoNumbers([1, 2, 3], [1, 8, 6, 2])); // [2, 10, 9] ==  [2, 0, 0, 1] 
