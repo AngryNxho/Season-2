3b5a39c70947f93373e0743d9ebd8c33e73b5dca
AngryNxho <pleastrixcl@gmail.com>
2023-07-12 17:19:55 -0400

    i cant understand

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -7,27 +7,31 @@ function TwoNumbers(arrOne, arrTwo){
   ? len = arrOne.length
   : len = arrTwo.length;
 
+// 2, 10, 9, 10 == 1, 0, 0, 0, 2
 
 for (let x = 0; x < len; x++){
+  (isNaN(arrOne[x]))
+  ? arrOne[x] = 0
+  : arrTwo[x] = 0;
+  
   operation = arrOne[x] + arrTwo[x];
-  console.log(arrOne == undefined, arrTwo == undefined);
-  if (9 >= operation ){
-    console.log(`Entra en el primer if con ${operation} `);
+
+  if (operation <= 9 && operation >= 0){
     total.push(operation)
-  }
-
-    if (operation > 9){
-      operation = 0;
-      total.push(operation);
-      console.log(`entra en segundo if con ${operation}`);
+  } 
+  else{
+      total.push(0)
+      sum += 1;
     }
+    
+    console.log(sum);  
 
-}
+} 
+
 
 console.log(total);
 
   return "";
 }
 
-
-console.log(TwoNumbers([1, 10, 9, 10, 4], [0, 0, 0, 0])); // 2, 0, 0, 1
+console.log(TwoNumbers([1, 10, 9, 10], [0, 0, 0, 0])); // 2, 0, 0, 1
