c6936be9d756add56b93c164cc0453bba362d40a
AngryNxho <pleastrixcl@gmail.com>
2023-07-13 12:08:35 -0400

    asdfsafd

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -1,9 +1,12 @@
 function TwoNumbers(arrOne, arrTwo) {
-  let sum = 0;
   let total = [];
+  let sum = 0;
 
+  arrOne.reverse();
+  arrTwo.reverse();
+  
   arrOne.length > arrTwo.length ? (len = arrOne.length) : (len = arrTwo.length);
-
+  
   for (z of String(len)) {
     for (let i = 0; i < len; i++) {
       if (isNaN(arrOne[i])){
@@ -13,24 +16,24 @@ function TwoNumbers(arrOne, arrTwo) {
       }
       
       op = arrOne[i] + arrTwo[i];
-      if (op > 9 && sum > 0) {
-        console.log(`${op} es mayor que 9`);
-        sum += 1;
-      }
-
       if (op <= 9 && op >= 0) {
         total.push(op);
         // console.log(op == 9);
-      } else if (op > 9 || op < 0) {
+      }
+       else if (op > 9 || op < 0) {
         op = 0;
+        sum += 1;
         total.push(op);
       }
-
-      console.log(op + sum);
     }
   }
 
-  console.log(total, sum);
-  return "";
+for (let z = 0; z < total.length; z++){
+  if (total[z] > 9 && total[z] < 0){
+    console.log(2);
+  }
+}  
+return "";
 }
-console.log(TwoNumbers([1, 9, 9, 0], [1, 1, 0, 0])); // 2, 0, 0, 1
+console.log(TwoNumbers([2, 4, 3], [5, 6, 4])); // 2, 0, 0, 0, 0, 1;
+// console.log(TwoNumbers([0], [0])); // 2, 0, 0, 0, 0, 1;
