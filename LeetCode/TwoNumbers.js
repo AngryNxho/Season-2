b393c873373ea09401ad1569ced00c1296c57eb3
AngryNxho <pleastrixcl@gmail.com>
2023-07-13 01:44:55 -0400

    d

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -1,34 +1,36 @@
-function TwoNumbers(arrOne, arrTwo){
-
+function TwoNumbers(arrOne, arrTwo) {
   let sum = 0;
   let total = [];
 
+  arrOne.length > arrTwo.length ? (len = arrOne.length) : (len = arrTwo.length);
 
-  (arrOne.length > arrTwo.length)
-  ? len = arrOne.length
-  : len = arrTwo.length;
-
-
-for (z of String(len)){
-  for (let i = 0; i < len; i ++){
-    isNaN(arrOne[i])
-    ? arrOne[i] = 0
-    : arrTwo[i] = 0;
-    op = arrOne[i] + arrTwo[i];
-
-    if (op <= 9 && op >= 0){
-      total.push(op);
-      console.log(op == 9);
+  for (z of String(len)) {
+    for (let i = 0; i < len; i++) {
+      if (isNaN(arrOne[i])){
+        arrOne.push(0);
+      }else{
+        arrTwo.push(0)
+      }
       
-    }else if (op > 9 || op < 0){
-      op = 0;
-      total.push(op)
+      op = arrOne[i] + arrTwo[i];
+      if (op > 9 && sum > 0) {
+        console.log(`${op} es mayor que 9`);
+        sum += 1;
+      }
+
+      if (op <= 9 && op >= 0) {
+        total.push(op);
+        // console.log(op == 9);
+      } else if (op > 9 || op < 0) {
+        op = 0;
+        total.push(op);
+      }
+
+      console.log(op + sum);
     }
   }
-}
 
-  console.log(total);
-
+  console.log(total, sum);
   return "";
 }
 console.log(TwoNumbers([1, 9, 9, 0], [1, 1, 0, 0])); // 2, 0, 0, 1
