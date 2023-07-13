5dd657d55990c49d276507f2da30066a1931f540
AngryNxho <pleastrixcl@gmail.com>
2023-07-13 01:22:25 -0400

    la mordidita

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -2,36 +2,33 @@ function TwoNumbers(arrOne, arrTwo){
 
   let sum = 0;
   let total = [];
-  
-  arrOne.length > arrTwo.length 
+
+
+  (arrOne.length > arrTwo.length)
   ? len = arrOne.length
   : len = arrTwo.length;
 
-// 2, 10, 9, 10 == 1, 0, 0, 0, 2
 
-for (let x = 0; x < len; x++){
-  (isNaN(arrOne[x]))
-  ? arrOne[x] = 0
-  : arrTwo[x] = 0;
-  
-  operation = arrOne[x] + arrTwo[x];
+for (z of String(len)){
+  for (let i = 0; i < len; i ++){
+    isNaN(arrOne[i])
+    ? arrOne[i] = 0
+    : arrTwo[i] = 0;
+    op = arrOne[i] + arrTwo[i];
 
-  if (operation <= 9 && operation >= 0){
-    total.push(operation)
-  } 
-  else{
-      total.push(0)
-      sum += 1;
+    if (op <= 9 && op >= 0){
+      total.push(op);
+      console.log(op == 9);
+      
+    }else if (op > 9 || op < 0){
+      op = 0;
+      total.push(op)
     }
-    
-    console.log(sum);  
+  }
+}
 
-} 
-
-
-console.log(total);
+  console.log(total);
 
   return "";
 }
-
-console.log(TwoNumbers([1, 10, 9, 10], [0, 0, 0, 0])); // 2, 0, 0, 1
+console.log(TwoNumbers([1, 9, 9, 0], [1, 1, 0, 0])); // 2, 0, 0, 1
