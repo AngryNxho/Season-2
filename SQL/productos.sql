344ba3fe08b8c70541bf1d741e24994941415a6b
= <=>
2023-07-23 16:11:18 -0400

    i dont know why

--- SQL/productos.sql
+++ SQL/productos.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS productos (
-  id int NOT NULL,
-  usuario_id int NOT NULL,
+  id SERIAL PRIMARY KEY,
   nombre varchar(80),
   precio_usd int
 );
@@ -9,13 +8,13 @@ CREATE TABLE IF NOT EXISTS productos (
 -- Insertando datos en tabla 'productos'
 --
 
-INSERT INTO productos (id, usuario_id, nombre, precio_usd) VALUES
-(1, 10, 'Clase Maestra de HTML', 39),
-(2, 10, 'Clase Maestra de CSS', 49),
-(3, 10, 'Clase Maestra de JavaScript', 59),
-(5, 10, 'Clase Maestra del DOM', 49),
-(6, 10, 'Herramientas Frontend', 39),
-(4, 12, 'Ruta Frontend', 199),
-(7, 15, 'Clase Maestra de HTML', 39),
-(8, 16, 'Clase Maestra de HTML', 39);
+INSERT INTO productos (nombre, precio_usd) VALUES
+('Clase Maestra de HTML', 39),
+('Clase Maestra de CSS', 49),
+('Clase Maestra de JavaScript', 59),
+('Clase Maestra del DOM', 49),
+('Herramientas Frontend', 39),
+('Ruta Frontend', 199),
+('Clase Maestra de HTML', 39),
+('Clase Maestra de HTML', 39);
 
