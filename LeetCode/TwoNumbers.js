38404825952e32c395576c212fcc7d85d5ee0531
AngryNxho <estebanmodatex@gmail.com>
2023-07-24 19:48:38 -0400

    aha

--- LeetCode/TwoNumbers.js
+++ LeetCode/TwoNumbers.js
@@ -13,30 +13,30 @@ function TwoNumbers(arrOne, arrTwo) {
         arrTwo.push(0)
       }
       op = Number(arrOne[i] + arrTwo[i]);
-
-      
-
-      if (op > 9 || op <  0){
+      if (op <= 9 && op >= 0){
+        if (i > 0 && )
+        let opSum = (op + sum - (sum - 1))
+        console.log(`numero: ${op} suma: ${opSum}`);
+      }else if (op > 9){
+        op += sum - (sum - 1)
+        sum++;
+        checkLen = (String(op).length);
         if (i == 0){
-          sum = 0;
+          total.push(Number(String(op)[checkLen - 1]));  
         }
-      }else {
-        console.log(sum);
-        op = String(op);
-        total.push(Number(op[op.length - 1]));
-  
+
       }
+
     }
     
 
 
-    console.log(total, sum);
 
     return ""; 
     // `total: ${total} - Adicionador: ${sum}`;
   }
   console.log(TwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
-  // console.log(TwoNumbers([2, 4, 3], [5, 6, 4]));
+  console.log(TwoNumbers([2, 4, 3], [5, 6, 4])); // 8, 0, 7
   //8,9,9,9,0,0,0,1
 
 
--- NodeJs/02 Proceso/Argumentos.js
+++ NodeJs/02 Proceso/Argumentos.js
@@ -1,1 +1,10 @@
-console.log(process.argv);
+// console.log(process.argv);
+
+function obtenerValor(parametro){
+    const i = process.argv.indexOf(parametro);
+    return process.argv[i + 1];
+
+}
+
+console.log(obtenerValor('install'));
+console.log(obtenerValor('-D'));
