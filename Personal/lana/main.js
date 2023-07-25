7601e09ae3eae4e7457f3b06239bf2b996d62f09
AngryNxho <estebanmodatex@gmail.com>
2023-07-25 17:40:44 -0400

    fas

--- Personal/lana/main.js
+++ Personal/lana/main.js
@@ -3,37 +3,30 @@ const left = document.querySelector(".left");
 const del = document.querySelector(".delete");
 const lastIndex = document.querySelector(".lastIndex");
 const content = document.querySelector(".content");
+const content2 = document.querySelector(".content").style;
 
-let addStyle = document.querySelector(".borderColor");
 let element;
 let leftCounter = 1;
 let rightCounter = 1;
 let positions = [];
 
+right.addEventListener("click", function addRight() {
+  element = document.createElement("button");
+  element.textContent = ` ${rightCounter}R`;
+  element.style.border = "1px solid black"; // Set the border style
+  element.style.height = "40px";
+  element.style.width = "50px";
 
+  content.style.color = "#7209b7";
+  rightCounter++;
+  positions.push(element.textContent);
 
-right.addEventListener("click", function addRight(){
-    element = document.createElement("p");
-    element.textContent = ` ${rightCounter}R`;
-    positions.push(element.textContent)
-    rightCounter++;
-    content.append(positions[positions.length - 1]);
-    document.body.classList.add("borderColor");
-    
-});
-
-left.addEventListener("click", function addLeft(){
-    element = document.createElement("p");
-    element.textContent = ` ${leftCounter}L`;
-    positions.push(element.textContent)
-    leftCounter++;
-    content.append(positions[positions.length - 1]);
-});
-
-del.addEventListener("click", function del(){
-    console.log("del");    
-}); 
-
-lastIndex.addEventListener("click", function last(){
-    console.log("last");    
+  if (positions.length % 11 === 1) {
+    // Create a new row after every 10 elements
+    content.style.gridAutoFlow = "row";
+  }
+  element.style.borderRadius = "10px";
+  content.style.gridTemplateColumns = `repeat(${Math.min(positions.length, 10)}, 1fr)`;
+  content.append(element);
+  console.log(positions[positions.length - 1]);
 });
