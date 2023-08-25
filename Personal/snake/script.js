5c85b3bd0b52d15ed26215427738fdf4a0bc2b94
AngryNxho <pleasetrixcl@gmail.com>
2023-08-25 02:21:06 -0400

    summit

--- Personal/snake/script.js
+++ Personal/snake/script.js
@@ -1,24 +1,20 @@
-const snake = document.getElementById("snake")
+const snake = document.getElementById("snake");
 let horizontalSize = 0;
 let verticalSize = 0;
-function test(e, margin){
+function test(e){
     // console.log(e.key);
     
     if (e.key === "ArrowRight"){
         horizontalSize += 25
-        console.log(e.key);
+        snake.style.marginLeft = horizontalSize
+        console.log(horizontalSize);
     }else if (e.key === "ArrowLeft"){
         horizontalSize -= 25
-        console.log(e.key);
-    }else if (e.key === "ArrowUp"){
-        verticalSize -= 25;
-    }else{
-        verticalSize += 25;
-
+        snake.style.marginRight = horizontalSize
+        console.log(horizontalSize);
     }
-    snake.style.marginLeft = horizontalSize
-    snake.style.marginTop = verticalSize
-}
 
+    
+}
 document.addEventListener("keydown", test);
 
