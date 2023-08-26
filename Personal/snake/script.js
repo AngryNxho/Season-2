40801259fd887d0fe5c8a67ffc3e5a821ea70dd1
AngryNxho <pleasetrixcl@gmail.com>
2023-08-26 04:56:07 -0400

    seems to be working now

--- Personal/snake/script.js
+++ Personal/snake/script.js
@@ -2,19 +2,24 @@ const snake = document.getElementById("snake");
 let horizontalSize = 0;
 let verticalSize = 0;
 function test(e){
-    // console.log(e.key);
+    console.log(e.key);
     
     if (e.key === "ArrowRight"){
-        horizontalSize += 25
-        snake.style.marginLeft = horizontalSize
-        console.log(horizontalSize);
-    }else if (e.key === "ArrowLeft"){
-        horizontalSize -= 25
-        snake.style.marginRight = horizontalSize
-        console.log(horizontalSize);
+        horizontalSize += 25;
+        console.log(horizontalSize)
     }
+    else if (e.key === "ArrowLeft"){
+        horizontalSize -= 25;
+        console.log(horizontalSize)
+    }else if (e.key ==="ArrowUp"){
+        verticalSize += 25;
+        console.log(verticalSize);
+    }else if (e.key === "ArrowDown"){
+        verticalSize -= 25;
+        console.log(verticalSize);
+    }
+    snake.style.marginLeft = horizontalSize;
+    snake.style.marginBottom = verticalSize;
 
-    
 }
-document.addEventListener("keydown", test);
-
+document.body.addEventListener("keydown", test);
