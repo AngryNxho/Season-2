b1aee17e75b70fa791ae73f204365c28a2e600fd
AngryNxho <pleasetrixcl@gmail.com>
2023-08-26 23:23:52 -0400

    hola

--- Personal/snake/script.js
+++ Personal/snake/script.js
@@ -1,24 +1,30 @@
 const snake = document.getElementById("snake");
 const box = document.querySelector(".box");
+const start = document.querySelector(".start");
 let extra = document.querySelector(".extraW");
 let horizontalSize = 0;
 let verticalSize = 0;
-function test(e){
-    console.log(e.key);
+
+function marginXY(){
+    counter = Math.trunc(Math.random() * 2) + 1;
+    console.log(counter);
+}
+
+function movement(e){
     extra.style.marginLeft= Math.trunc(Math.random() * 450);
     if (e.key === "ArrowRight"){
         horizontalSize += 25;
-        console.log(horizontalSize)
+        // console.log(horizontalSize)
     }
     else if (e.key === "ArrowLeft"){
         horizontalSize -= 25;
-        console.log(horizontalSize)
+        // console.log(horizontalSize)
     }else if (e.key ==="ArrowUp"){
         verticalSize += 25;
-        console.log(verticalSize);
+        // console.log(verticalSize);
     }else if (e.key === "ArrowDown"){
         verticalSize -= 25;
-        console.log(verticalSize);
+        // console.log(verticalSize);
     }
     snake.style.marginLeft = horizontalSize;
     snake.style.marginBottom = verticalSize;
@@ -31,4 +37,14 @@ function test(e){
     }
     console.log(Math.trunc(Math.random() * 450));
 }
-document.addEventListener("keydown", test);
+snake.classList.add("hidden");
+extra.classList.add("hidden");
+document.addEventListener("keydown", movement);
+
+start.addEventListener("click", function(){
+    snake.classList.remove("hidden")
+    extra.classList.remove("hidden");
+
+});
+
+document.addEventListener("click", marginXY);
