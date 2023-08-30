c9a108568bf1a2f24bbe9520d909db092fe9385a
AngryNxho <pleasetrixcl@gmail.com>
2023-08-30 17:38:58 -0400

    dsd

--- Personal/snake/script.js
+++ Personal/snake/script.js
@@ -17,37 +17,43 @@ function randomXY(){
     return random;
 }
 
-
 function movement(e){
-    if (e.key === "ArrowRight"){
+    let key = e.key;
+    console.log(key, 123123);
+    if (key === "ArrowRight"){
         horizontalSize += 25;
         console.log(horizontalSize)
     }
-    else if (e.key === "ArrowLeft"){
+    else if (key === "ArrowLeft"){
         horizontalSize -= 25;
         console.log(horizontalSize)
-    }else if (e.key ==="ArrowUp"){
+    }else if (key ==="ArrowUp"){
         verticalSize += 25;
         console.log(verticalSize);
     }else{
         verticalSize -= 25;
         console.log(verticalSize);
     }
-    
-    let horizontalDir = (Math.trunc(Math.random() * randomXY()) + 1);
-        snake.style.width = 25;
+
     snake.style.marginLeft = horizontalSize;
     snake.style.marginBottom = verticalSize;
-    if (horizontalSize >= 450 || horizontalSize <= -450){
-        horizontalSize = 0;
-        verticalSize = 0;
-    }else if (verticalSize >= 450 || verticalSize <= -450){
-        verticalSize = 0;
-        horizontalSize = 0;
-    }
-    // console.log(Math.trunc(Math.random() * 450));
-    
-    return horizontalDir;
+
+}
+
+function test() {
+    let horizontalDir = (Math.trunc(Math.random() * randomXY()) + 1);
+    snake.style.width = 25;
+if (horizontalSize >= 450 || horizontalSize <= -450){
+    horizontalSize = 0;
+    verticalSize = 0;
+}else if (verticalSize >= 450 || verticalSize <= -450){
+    verticalSize = 0;
+    horizontalSize = 0;
+}
+console.log(Math.trunc(Math.random() * 450));
+// 
+return key;
+
 }
 snake.classList.add("hidden");
 extra.classList.add("hidden");
@@ -64,5 +70,4 @@ start.addEventListener("click", function(){
 document.addEventListener("click", function(){
     extra.style.marginLeft = 150;
     console.log(extra.style.marginLeft);
-
 })
