d570a1119c559ca3b45b8a47cfacf84b0a8a2a9e
= <=>
2023-10-22 12:50:36 -0300

    x

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
-function test(value) {
-    let c = 2;
-    for (let x = 0; x < value; x++){
-        c+= 3
-    }
-    return c;
-}
 
-console.log(test(13));
+var isPalindrome = function(x) {
+    let save = String(x);
+    c = 0;
+    for (let [z, d] = [0, 1]; z < save.length; z++, d++){
+        if ((save[z]) == String(save[save.length - d])) {
+            c++;
+        }
+        console.log( String(save[save.length - d]));
+
+    }
+
+
+    if (c == save.length) {
+        return true
+    }else {
+        return false
+    }
+
+};
