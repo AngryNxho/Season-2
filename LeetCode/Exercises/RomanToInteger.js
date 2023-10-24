821b556179a883324dd3777c2f0f4ec8eb1f55e1
= <=>
2023-10-24 06:48:47 -0300

    d

--- LeetCode/Exercises/RomanToInteger.js
+++ LeetCode/Exercises/RomanToInteger.js
@@ -3,10 +3,6 @@ const romanToInt = function(s) {
     //Written from left to right
 
   
-// I puede colocarse antes de V (5) y X (10) para hacer 4 y 9.
-// X puede colocarse antes de L (50) y C (100) para hacer 40 y 90.
-// C puede colocarse antes de D (500) y M (1000) para hacer 400 y 900.
-// Dado un número romano, conviértelo a un número entero.
 
     const romans = {
         "I":1,
@@ -20,18 +16,31 @@ const romanToInt = function(s) {
 
     let result = 0;
 
-    for (z in s) {
-        for (let x = 0; x < Object.values(romans).length; x++){
-            if (s[x] == Object.keys(romans)[z]){
-                console.log(s[x], Object.keys(romans)[z]);
-                result += Object.values(romans)[z]
-            }
-            // result += Object.values(romans)[x]
+    for (let x = 0; x < Object.values(romans).length; x++){
+        for (z in s) {
+        
+            console.log((s[z] =="I"), (s[s.length - z] == "X"));
+             if ((s[z] == Object.keys(romans)[x]) && (result < s.length) ){
+                 console.log("Primer if");
+                 result += (Object.values(romans)[x]);
+             }
 
+             else if ((s[z] =="I") && (result < s.length)){
+                 console.log("else IF");
+                 result += Object.values(romans)[z] - 1;
+             }
+            // else if ((s[x] == "L" && s[x+1] == "C") && (result < s.length)){
+            //     result += Object.values(romans)[z] - 1;
+            //     console.log("Segundo if");
+            // }
+
+            
         }
     }
     return result;
 };
 
-
-console.log(romanToInt("MCMXCIV"))
+// I puede colocarse antes de V (5) y X (10) para hacer 4 y 9.
+// X puede colocarse antes de L (50) y C (100) para hacer 40 y 90.
+// C puede colocarse antes de D (500) y M (1000) para hacer 400 y 900.
+console.log(romanToInt("X"))
