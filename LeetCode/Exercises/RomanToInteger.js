0cd93062a99782096114ec8510e740338204bf29
AngryNxho <pleasetrixcl@gmail.com>
2023-10-24 15:55:38 -0300

    d

--- LeetCode/Exercises/RomanToInteger.js
+++ LeetCode/Exercises/RomanToInteger.js
@@ -12,15 +12,16 @@ const romanToInt = function(s) {
         "M":1000
     }
 
-    let result = "";
+    let result = 0;
 
     let i = 0; 
     for (let z in s) {
         for (const [key, value] of Object.entries(romans)) {
-            console.log(key, s[z], key == s[z]);
-            if ( key == s[z]){
+            // console.log(key, s[z], key == s[z]);
+            if (key == s[z]){
                 result += value;
             }
+
         }
     
     }
@@ -32,4 +33,4 @@ const romanToInt = function(s) {
 // I puede colocarse antes de V (5) y X (10) para hacer 4 y 9.
 // X puede colocarse antes de L (50) y C (100) para hacer 40 y 90.
 // C puede colocarse antes de D (500) y M (1000) para hacer 400 y 900.
-console.log(romanToInt("MX"))
+console.log(romanToInt("IV"))
