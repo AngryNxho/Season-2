9998925c982d5f10757c1f6c3cd4bc7f910e6275
= <=>
2023-10-24 21:29:58 -0300

    f

--- LeetCode/Exercises/RomanToInteger.js
+++ LeetCode/Exercises/RomanToInteger.js
@@ -14,18 +14,12 @@ const romanToInt = function(s) {
 
     let result = 0;
 
-    let i = 0; 
+    let i = 0
     for (let z in s) {
         for (const [key, value] of Object.entries(romans)) {
-            // console.log(key, s[z], key == s[z]);
-            if (key == s[z]){
-                result += value;
-            }
-
+            console.log((key == s[z]) && (s[z] == "V" || s[z] == "X" ) && (s[i] == "I"));
         }
-    
-    }
-    
+     }
     return result;
 };
 
@@ -33,4 +27,21 @@ const romanToInt = function(s) {
 // I puede colocarse antes de V (5) y X (10) para hacer 4 y 9.
 // X puede colocarse antes de L (50) y C (100) para hacer 40 y 90.
 // C puede colocarse antes de D (500) y M (1000) para hacer 400 y 900.
-console.log(romanToInt("IV"))
+console.log(romanToInt("VI"));
+
+// if ((key == s[z]) && (s[z] == "V" || s[z] == "X" ) && (s[i] == "I")){
+    //     console.log("Primer if");
+    //     result += value - 1;
+    // }
+    // i++
+   
+
+ // else if ((key == s[z]) && (s[z] == "L" || s[z] == "C"  &&  s[i] == "X")) {
+                //     console.log("Segundo if");
+                //     result += value - 10;
+                // }else if (((key == s[z]) && (s[z] == "D" || s[z] == "M"  &&  s[i] == "C"))) {
+                    //     console.log("Tercer if");
+                    //     result += value - 100;
+                    // }else if (key == s[z]){
+                        //     result += value;
+                        //
