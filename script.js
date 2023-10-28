3bfd9c391104fffd95a3505dffb2a4e074be6213
= <=>
2023-10-28 08:38:33 -0300

    j

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
-function getRandomNumber(max) {
-    let arr = [];
-    let randomNumber;
-    for (let x = 0; x < Number(max); x++) {
-        randomNumber = Math.trunc(Math.random() * 9) + 1;
-        if (!(arr.includes(randomNumber))) {
-            arr.push(randomNumber);
-        }else {
-            x--;
-        }
-    }
-    
-    let toques = 0;
+let someValue = "value from outer scope";
 
-    for (z of arr) {
-        for (let i = 0; i < arr.length; i++){
-            if (z == arr[i]) {
-                toques++;
-                toques.textContent = 11234
-            }
-
-        }
-    }
-
-    return arr;
+function innerFunction(parameter) {
+    console.log("Parameter received:", parameter);
 }
+
+function outerFunction() {
+    innerFunction(someValue);
+}
+
+outerFunction();  // logs: Parameter received: value from outer scope
