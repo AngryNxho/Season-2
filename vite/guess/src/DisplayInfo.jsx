255e3671c916dc99f12bdbe0ee4fbc9fb6e1dea9
= <=>
2023-10-29 12:10:32 -0300

    asfd

--- vite/guess/src/DisplayInfo.jsx
+++ vite/guess/src/DisplayInfo.jsx
@@ -16,23 +16,41 @@ function getRandomNumbers(numbers) {
 }
 
 
-export default function DisplayInfo() { 
-    const [length, setLength] = useState("");
 
+
+export default function DisplayInfo() { 
+
+    const [inputValue, setInputValue] = useState("");
+    const [guess, setGuess] = useState("");
 
     const [toque, setToque] = useState(0);
     const [fama, setFama] = useState(0);
 
     
     
-    const handleChange = (event) => {
-        let secretCode = getRandomNumbers(event.target.value) ;
+    const handleChangeInput = (event) => {
+        if ((event.target.value).length == 1) {
+            setInputValue(event.target.value);
+        }else {
+            setInputValue("")
+        }
+    };
 
-        console.log(secretCode, event.target.value)
-        setLength(event.target.value);
-        
-      };
 
+    const handleChangeGuess = (event) => {
+        setGuess(event.target.value)
+        console.log(guess)
+    };
+
+    const handleClick = (e) => {
+        const secretCode = getRandomNumbers(inputValue);
+        for (let x in secretCode) {
+            console.log(x)
+        }
+    }
+
+    
+    
    
 
     return (
@@ -40,15 +58,15 @@ export default function DisplayInfo() {
             <div className = "container">
                 <div className="content">
                     <label>Number Length</label>
-                    <input type="number" className="length" min = {1} max={9} onChange={handleChange}/>
+                    <input type="number" className="length" min = {1} max={9} maxLength={1} onChange={handleChangeInput} value = {inputValue} placeholder="Choose a number from 1 to 9"/>
                     <label>Guess the Number</label>
-                    <input type="number" className="numbers"/> 
+                    <input type="number" className="numbers" placeholder="Guess" onChange={handleChangeGuess}/> 
                     <div className="centerDiv">
-                        <h1 className = "secret">?</h1>
+                        <h2 className = "secret">?</h2>
                     </div>
                     <div className="btns">
                         <button className="begin">Start</button>
-                        <button className="check">check</button>
+                        <button className="check" onClick={handleClick}>check</button>
                     </div>
                     <div className="stats">
                         <h1>Toques: {toque}</h1>
