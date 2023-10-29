cb6b75459c22d14e82967aa82e1ee862e76105c9
= <=>
2023-10-29 17:24:30 -0300

    f

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function getRandomNumbers(numbers) {
-    let arr = [];
-    console.log(numbers);
-    
-    for (let x = 0; x < numbers; x++) {
-        let random = Math.trunc(Math.random() * 9 + 1);
-        if (!(arr.includes(random))) {
-            arr.push(random);
-        }else {
-            x--;
-        }
-    }
-
-    return arr
+.-:_:_;[][¨*=?=?1|1|for (let x in 37) {
+    console.log(x)
 }
-
-console.log(getRandomNumbers(5));
