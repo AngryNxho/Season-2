4954ff8043cedca2a364fe5fa6e5f60447f56a85
= <=>
2023-10-31 22:46:40 -0300

    s

--- vite/guess/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import DisplayInfo from "./DisplayInfo";
-import "./styles.css"
-export default function App() {
-    return (
-        <DisplayInfo/>
-    )
-}
---
+++ vite/whatever/src/App.jsx
@@ -0,0 +1,13 @@
+import ImageList from "./Components/ImageList"
+import SearchBar from "./Components/SearchBar"
+import ImageShow from "./Components/ImageShow"
+
+export default function App() {
+    return (
+        <div>
+            <ImageList />
+            <SearchBar />
+            <ImageShow />
+        </div>
+    )
+}
