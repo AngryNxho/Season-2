fdbeab3000dce5c91be439da6c4ac0a383b0bda6
AngryNxho <AngryNxho>
2023-11-30 17:14:26 -0300

    dzsfg

--- Odin/Library/script.js
+++ Odin/Library/script.js
@@ -1,8 +1,11 @@
 const myLibrary = [];
 const addBook = document.getElementById("add");
+const btnContainer = document.querySelector(".addBtnContainer")
 
 function addBookToLibrary() {
     
 }
 
-addBook.addEventListener("click", addBookToLibrary)
+addBook.addEventListener("click", () => {
+    btnContainer.appendChild(123);
+})
