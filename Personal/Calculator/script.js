4c7e3b3af9583c637e37ea41bac2c945d15d4cd4
AngryNxho <AngryNxho>
2023-12-14 17:17:23 -0300

    fdas

--- Personal/Calculator/script.js
+++ Personal/Calculator/script.js
@@ -1,9 +1,19 @@
 const allBtns = document.querySelectorAll(".btns");
 const input = document.querySelector(".input");
-
+const zero = document.querySelector(".zero");
+const reset = document.querySelector(".del");
+const fakeZero = document.querySelector(".hide");
+let inputNums = [];
 
 allBtns.forEach(allBtns => {
-    allBtns.addEventListener("click", () => {
-        input.textContent = (allBtns.textContent)
+    allBtns.addEventListener("click", (e) => {
+        inputNums.push(allBtns.textContent);
+        
+        input.textContent += inputNums[inputNums.length - 1];
     })
+})
+
+
+reset.addEventListener("click", () => {
+    input.textContent = "0"
 })
