cbcc2665233a1d3ca5daa7e514d3d37edcdfa39e
AngryNxho <AngryNxho>
2023-12-15 23:31:18 -0300

    asdf

--- Personal/Calculator/script.js
+++ Personal/Calculator/script.js
@@ -3,17 +3,21 @@ const input = document.querySelector(".input");
 const zero = document.querySelector(".zero");
 const reset = document.querySelector(".del");
 const fakeZero = document.querySelector(".hide");
-let inputNums = [];
+let inputNums = [0];
+input.textContent = 0;
 
 allBtns.forEach(allBtns => {
+    input.textContent = "";
     allBtns.addEventListener("click", (e) => {
         inputNums.push(allBtns.textContent);
-        
-        input.textContent += inputNums[inputNums.length - 1];
+        console.log(input.textContent == 0)
+        if (inputNums.length <= 6) {
+            input.textContent += inputNums[inputNums.length - 1];
+        }
     })
 })
 
-
 reset.addEventListener("click", () => {
-    input.textContent = "0"
+    inputNums = [];
+    input.textContent = 0;
 })
