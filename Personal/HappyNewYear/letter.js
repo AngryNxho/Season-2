157b8a916f8bbf696cf5467f141901846dba3843
AngryNxho <pleasetrixcl@gmail.com>
2023-12-26 04:13:39 -0300

    s

--- Personal/HappyNewYear/letter.js
+++ Personal/HappyNewYear/letter.js
@@ -1,5 +1,9 @@
-document.getElementById("openLetterButton").addEventListener("click", function() {
-    var letter = document.getElementById("letter");
-    letter.classList.toggle("hidden");
-    letter.classList.toggle("visible");
+const letter = document.querySelector(".closed img"); // Select the img inside the anchor tag with class 'closed'
+
+letter.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default action of the anchor tag
+    letter.src = "./openLetter.png"; // Change the src of the img
+    setTimeout(() => {
+        letter.classList.add("hidden")
+    }, 1000);
 });
