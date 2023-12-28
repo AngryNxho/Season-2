ef81a4ce24fa75ef6cd8bfbb5d9b5050367908cb
AngryNxho <pleasetrixcl@gmail.com>
2023-12-28 03:10:31 -0300

    asdf

--- Personal/HappyNewYear/letter.js
+++
@@ -1,9 +0,0 @@
-const letter = document.querySelector(".closed img"); // Select the img inside the anchor tag with class 'closed'
-
-letter.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent the default action of the anchor tag
-    letter.src = "./openLetter.png"; // Change the src of the img
-    setTimeout(() => {
-        letter.classList.add("hidden")
-    }, 1000);
-});
